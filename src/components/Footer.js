80b51b76baa3d6a373d82d154a31d9c5a82492d2
Ovi Savescu <ovi@ovswebsites.com>
2021-08-07 07:44:02 +0300

    footer finalized

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ const Footer = () => {
                 src="../images/logos/DG2GO-logo-transparent-white.png"
                 alt="DG2Go Logo"
                 placeholder="tracedSVG"
-                width={200}
+                width={201}
               />
             </a>
           </div>
@@ -56,78 +56,57 @@ const Footer = () => {
             </p>
             <ul>
               <li>
-                <Link to="/passes/all-day-fun-pass/">All-Day Fun Pass</Link>
+                <Link to="/pizza-shop-menu/">DG2GO Pizza Shop</Link>
               </li>
               <li>
-                <Link to="/passes/sundowner-fun-pass/">Sundowner Pass</Link>
+                <Link to="/delgrosso-dinners-to-go/">DG2GO Meals</Link>
               </li>
               <li>
-                <Link to="/passes/season-pass/">Season Pass</Link>
+                <Link to="/on-the-road/">DG2GO On the Road</Link>
               </li>
               <li>
-                <Link to="/ride-tickets/">Ride Tickets</Link>
+                <Link to="/specials/">DG2GO Specials</Link>
               </li>
             </ul>
           </div>
 
           {/* <!-- menu 2 --> */}
-
-          {/* <!-- menu 3 --> */}
           <div className="footer__col col3">
             <p className="uppercase text-sm tracking-wider mb-4 mt-6 font-semibold">
               About
             </p>
             <ul>
               <li>
-                <Link to="/hours-and-schedule-of-operation/">
-                  Hours &amp; Schedule
-                </Link>
+                <Link to="/history/">History</Link>
               </li>
               <li>
-                <Link to="/policies-info-and-faqs/">FAQs</Link>
+                <Link to="/join-our-team/">Join Our Team</Link>
               </li>
               <li>
-                <Link to="/famous-food/the-delgrosso-food-tradition/">
-                  Famous Food
-                </Link>
-              </li>
-
-              <li>
-                <Link to="/our-history/">History</Link>
-              </li>
-              <li>
-                <Link to="/employment/">Join Our Team</Link>
-              </li>
-              <li>
-                <Link to="/donation-requests/">Donation Requests</Link>
+                <Link to="/contact/">Contact Us</Link>
               </li>
             </ul>
           </div>
 
-          {/* <!-- menu 4 --> */}
+          {/* <!-- menu 3 --> */}
           <div className="footer__col col4">
             <p className="uppercase text-sm tracking-wider mb-4 mt-6 font-semibold">
               Legal
             </p>
             <ul>
               <li>
-                <Link to="/contact/">Contact Us</Link>
+                <Link to="/privacy-policy/">Privacy Policy</Link>
               </li>
               <li>
                 <Link to="/terms-and-conditions/">Terms &amp; Conditions</Link>
               </li>
               <li>
-                <Link to="/privacy-policy/">Privacy Policy</Link>
-              </li>
-              <li>
-                <Link to="/website-accessibility-statement/">
+                <Link to="/accessibility-statement/">
                   Accessibility Statement
                 </Link>
               </li>
               <li>
-                <Link to="/website-accessibility-policy/">
-                  Accessibility Policy
-                </Link>
+                <Link to="/accessibility-policy/">Accessibility Policy</Link>
               </li>
             </ul>
           </div>
@@ -142,7 +121,7 @@ const Footer = () => {
           ©2014-2020 DelGrosso’s Amusement Park, Inc. All Rights Reserved.
           <span className="hidden md:inline"> | </span>
           <span className="block mt-3 md:inline">
-            Website by:
+            Website by:{" "}
             <a
               href="https://studiorovst.com"
               target="_blank"
